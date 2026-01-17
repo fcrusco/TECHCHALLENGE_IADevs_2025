83501c307b6332d602587fc33864ebb2278951b3
Shioned <driverfelipe@gmail.com>
2026-01-17 01:04:45 -0300

    Update Azure
    Update Azure

---
+++ FelipeMoraes/Fase 2/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "=== Instalando dependencias ==="
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+echo "=== Iniciando aplicacao ==="
+python src/treinamento.py
